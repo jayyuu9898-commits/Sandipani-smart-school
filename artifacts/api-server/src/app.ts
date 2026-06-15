4be9419c83ff24fada400cbc4b45989e1b6cf005
jayyuu9898-commits <jayyuu9898@gmail.com>
2026-06-15 15:11:48 +0530

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -61,6 +61,7 @@ app.use("/api", router);
 // Serve frontend
 const frontendPath = path.join(process.cwd(), "../sandipani/dist/public");
 
+console.log("FRONTED PATH:", frontendpath);
 app.use(express.static(frontendPath));
 
 app.use((req, res) => {
