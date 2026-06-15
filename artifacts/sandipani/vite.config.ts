a9161ffc3ee6e105691e061645d07b69132e0efe
jayyuu9898-commits <jayyuu9898@gmail.com>
2026-06-15 21:59:35 +0530

    Update vite.config.ts

--- artifacts/sandipani/vite.config.ts
+++ artifacts/sandipani/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 const rawPort = process.env.PORT;
-const port = rawPort ? Number(rawPort) : 5173
+const port = rawPort ? Number(rawPort) : 5173;
 export default defineConfig({
   base:"/",
   plugins: [
